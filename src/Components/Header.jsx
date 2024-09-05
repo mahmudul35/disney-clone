d362209074ad20e324ff975671dad7c901d74645
mahmudul35 <101060541+mahmudul35@users.noreply.github.com>
2024-09-05 12:11:15 -0700

    header navbar added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,56 @@
 import React from "react";
 import logo from "./../assets/Images/logo.png";
+import profile from "./../assets/Images/profile.png";
+import {
+  HiHome,
+  HiMagnifyingGlass,
+  HiStar,
+  HiPlayCircle,
+  HiTv,
+} from "react-icons/hi2";
+import { HiPlus, HiDotsVertical } from "react-icons/hi";
 export const Header = () => {
-  return <div>Header</div>;
+  const menu = [
+    {
+      name: "HOME",
+      icon: HiHome,
+    },
+    {
+      name: "SEARCH",
+      icon: HiMagnifyingGlass,
+    },
+    {
+      name: "WATCH LIST",
+      icon: HiPlus,
+    },
+    {
+      name: "ORIGINALS",
+      icon: HiStar,
+    },
+    {
+      name: "MOVIES",
+      icon: HiPlayCircle,
+    },
+    {
+      name: "SERIES",
+      icon: HiTv,
+    },
+  ];
+  return (
+    <div className=" flex justify-between p-4 items-center">
+      <img className="w-[90px] lg:w-[115px] object-cover" src={logo} alt="" />
+
+      <div className="flex  items-center space-x-8">
+        {menu.map((item) => {
+          return (
+            <div className="flex items-center gap-2 text-white cursor-pointer font-semibold text-[15px] hover:underline underline-offset-8 ">
+              {<item.icon />}
+              <h2>{item.name}</h2>
+            </div>
+          );
+        })}
+      </div>
+      <img className="w-[60px]" src={profile} alt="" />
+    </div>
+  );
 };
